7ff3ac30a84ca3f9e1cddd5be8f96137981d01dc
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-10-03 10:11:56 -0400

    Segunda prueba con valores distintos

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -4,6 +4,9 @@ describe("Totalizador ", () => {
   it("Deberia calcular subtotal para una cantidad", () => {
     expect(calcularTotal(2, 1, "")).toEqual(2);
   });
+  it("Deberia calcular subtotal para una cantidad", () => {
+    expect(calcularTotal(5, 3, "")).toEqual(15);
+  });
 });
 
 
