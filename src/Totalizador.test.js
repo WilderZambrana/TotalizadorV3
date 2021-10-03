935c2bc5a21bcf6131c4b659b2082830adf395b8
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-10-03 10:27:22 -0400

    Iniciando 4ta prueba, Calcular subtotal para un estado

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -10,6 +10,9 @@ describe("Totalizador ", () => {
   it("Deberia calcular subtotal para un estado por defecto", () => {
     expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
   });
+  it("Deberia calcular subtotal para un estado", () => {
+    expect(calcularTotal(3, 2, "UT")).toEqual(6.399);
+  });
 });
 
 
