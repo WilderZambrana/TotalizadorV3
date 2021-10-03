12605c25a6aa53cb1560edea96e12c84c324bd25
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-10-03 10:23:38 -0400

    Implementando codigo de produccion de la 3ra prueba

--- src/Totalizador.js
+++ src/Totalizador.js
@@ -1,6 +1,7 @@
 function calcularTotal(cantidad, precio, estado) {
-    
-    return cantidad*precio;
+    let impuesto=0;
+    if(estado == "CA") impuesto=0.0825;
+    return cantidad*precio+cantidad*precio*impuesto;
 }
 
 export default calcularTotal;
--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -7,7 +7,7 @@ describe("Totalizador ", () => {
   it("Deberia calcular subtotal para una cantidad", () => {
     expect(calcularTotal(5, 3, "")).toEqual(15);
   });
-  it("deberia calcular para un estado por defecto", () => {
+  it("Deberia calcular subtotal para un estado por defecto", () => {
     expect(calcularTotal(2, 2, "CA")).toEqual(4.33);
   });
 });
