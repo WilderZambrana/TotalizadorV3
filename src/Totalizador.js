7ea7495d39330e424dad5f7f5664735fb9c599eb
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-10-03 12:28:56 -0400

    4ra refactorizacion descuentos

--- src/Totalizador.js
+++ src/Totalizador.js
@@ -1,10 +1,8 @@
 function calcularTotal(cantidad, precio, estado) {
     let subtotal = subTotal(cantidad, precio);
-    let descuento = 0;
-    subtotal = subtotal + subtotal * impuestoEstado(estado);
-    if (subtotal > 1000) descuento = 0.03;
-    if (subtotal > 3000) descuento = 0.05;
-    return subtotal - subtotal * descuento;
+    let impuesto = subtotal * impuestoEstado(estado);    
+    subtotal = subtotal + impuesto;    
+    return subtotal - subtotal * descuento(subtotal);
 }
 
 function subTotal(cantidad, precio) {
@@ -12,15 +10,18 @@ function subTotal(cantidad, precio) {
 }
 
 function impuestoEstado(estado) {
-    /*let impuesto = 0;
-    if (estado == "UT") impuesto = 0.0665;
-    if (estado == "NV") impuesto = 0.08;
-    if (estado == "TX") impuesto = 0.0625;
-    if (estado == "AL") impuesto = 0.04;
-    if (estado == "CA") impuesto = 0.0825;
-    return impuesto;*/
     let impuestos = { "": 0, "UT": 0.0665, "NV": 0.08, "TX": 0.0625, "AL": 0.04, "CA": 0.0825 };
     return impuestos[estado];
+}
+
+function descuento(subtotal){
+    let descuento = 0;
+    if (subtotal > 1000) descuento = 0.03;
+    if (subtotal > 3000) descuento = 0.05;
+    if (subtotal > 7000) descuento = 0.07;
+    if (subtotal > 10000) descuento = 0.10;
+    if (subtotal > 15000) descuento = 0.15;
+    return descuento;
 }
 
 export default calcularTotal;
