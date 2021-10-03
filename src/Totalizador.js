d0fbf1af8459c227f8c7c39524e5474e21aeaa8a
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-10-03 10:29:27 -0400

    4ta prueba, codigo de produccion

--- src/Totalizador.js
+++ src/Totalizador.js
@@ -1,7 +1,8 @@
 function calcularTotal(cantidad, precio, estado) {
-    let impuesto=0;
-    if(estado == "CA") impuesto=0.0825;
-    return cantidad*precio+cantidad*precio*impuesto;
+    let impuesto = 0;
+    if (estado == "CA") impuesto = 0.0825;
+    if (estado == "UT") impuesto = 0.0665;
+    return cantidad * precio + cantidad * precio * impuesto;
 }
 
 export default calcularTotal;
